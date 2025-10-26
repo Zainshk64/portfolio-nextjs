94031cf19341faaa87e7eb50f6ef4cabd6afa624
Zain Shk <zshk599@gmail.com>
2025-10-26 16:43:36 +0500

    Hero Animation

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -3,7 +3,6 @@
 'use client'
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
-import { Player } from "@lottiefiles/react-lottie-player";
 
 const HeroImage = () => {
   return (
@@ -16,14 +15,7 @@ const HeroImage = () => {
         height={1000}
         width={1000}
       /> */}
-      {/* <div className="flex justify-center items-center">
-      <Player
-        autoplay
-        loop
-        src="/developer skills.lottie"
-        style={{ height: "300px", width: "300px" }}
-      />
-    </div> */}
+      
 
 
      <DotLottieReact
